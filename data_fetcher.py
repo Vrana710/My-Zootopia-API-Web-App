df05a46779b1617eeeb1264321b9d0be6b8ae1f8
Varsha Rana <73100366+Vrana710@users.noreply.github.com>
2024-08-05 21:00:00 +0200

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -8,6 +8,7 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 API_URL = 'https://api.api-ninjas.com/v1/animals?name='
 
+
 def fetch_data(animal_name):
     """
     Fetches the animals data for the animal 'animal_name'.
@@ -29,6 +30,7 @@ def fetch_data(animal_name):
         'X-Api-Key': API_KEY
     }
     response = requests.get(f"{API_URL}{animal_name}", headers=headers)
+
     
     if response.status_code == 200:
         return response.json()
